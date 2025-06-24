fcb3c2ab18e54dce2b9cf7f3c642e9c08da649bb
1brucben <1benjbruce@gmail.com>
2025-06-24 21:36:23 +0200

    UsernameSchema, FlagSchema (#1185)
    Fix zod validation failure for flag codes that do not conform to the
    SafeString pattern.
    
    - [x] I have added screenshots for all UI updates
    - [x] I process any text displayed to the user through translateText()
    and I've added it to the en.json file
    - [x] I have added relevant tests to the test directory
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    - [x] I understand that submitting code with bugs that could have been
    caught through manual testing blocks releases and new features for all
    contributors
    
    Co-authored-by: Scott Anderson <662325+scottanderson@users.noreply.github.com>

--- src/core/Schemas.ts
+++ src/core/Schemas.ts
@@ -175,10 +175,13 @@ export const AttackIntentSchema = BaseIntentSchema.extend({
   troops: z.number().nullable(),
 });
 
+export const UsernameSchema = SafeString;
+export const FlagSchema = z.string().max(128).optional();
+
 export const SpawnIntentSchema = BaseIntentSchema.extend({
-  flag: z.string().nullable(),
   type: z.literal("spawn"),
-  name: SafeString,
+  name: UsernameSchema,
+  flag: FlagSchema,
   playerType: PlayerTypeSchema,
   x: z.number(),
   y: z.number(),
@@ -335,8 +338,8 @@ export const ServerPrestartMessageSchema = ServerBaseMessageSchema.extend({
 
 export const PlayerSchema = z.object({
   clientID: ID,
-  username: SafeString,
-  flag: SafeString.optional(),
+  username: UsernameSchema,
+  flag: FlagSchema,
 });
 
 export const GameStartInfoSchema = z.object({
@@ -413,8 +416,8 @@ export const ClientJoinMessageSchema = z.object({
   token: TokenSchema, // WARNING: PII
   gameID: ID,
   lastTurn: z.number(), // The last turn the client saw.
-  username: SafeString,
-  flag: SafeString.optional(),
+  username: UsernameSchema,
+  flag: FlagSchema,
 });
 
 export const ClientMessageSchema = z.discriminatedUnion("type", [
